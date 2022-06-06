90d68ccb7ab444169274cbb505f61d1ca60ef4b0
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-06 14:53:53 -0400

    Uso del tipo de dato var

--- DataTypes/src/DataTypes.java
+++ DataTypes/src/DataTypes.java
@@ -6,5 +6,17 @@ public class DataTypes {
 
         double nD = 123.4567890123456789;
         float nF = 123.4567890123456789F;
+
+        //Es posible usar la keyword var para que el sistema deduzca el tipo de dato de la variable
+        var salary = 1000;
+        //pension -3%
+        var pension = salary*0.03;
+        var totalSalary = salary - pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName = "Marco Perez";
+        System.out.println("Employee Name: " + employeeName + " Salary: " + totalSalary);
     }
 }
