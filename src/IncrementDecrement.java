0c3a2c156b6db65e67720ca068f2f3f7eeaee705
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-06 15:04:38 -0400

    Operadores de Incremento y Decremento

---
+++ src/IncrementDecrement.java
@@ -0,0 +1,21 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int lives = 5;
+        lives = lives - 1;
+        System.out.println(lives);
+
+        //Postfija
+        lives--; //Decremento
+        System.out.println(lives);
+
+        lives++; //Incremento
+        System.out.println(lives);
+
+        //Prefija
+        //Gana un regalo por ganar una vida
+        int gift = 100 + ++lives;
+        System.out.println(gift);
+        System.out.println(lives);
+    }
+}
