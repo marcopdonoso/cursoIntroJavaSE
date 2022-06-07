812a1061281bc3b3be2ef197330c4df8ac388f03
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-07 15:14:57 -0400

    Java Docs aplicado a las Funciones de nuestra clase Functions

--- src/Functions.java
+++ src/Functions.java
@@ -20,18 +20,39 @@ public class Functions {
         System.out.println("Conversion a US$: " + convertToDolar(200,"MXN"));
     }
 
+    /**
+     * Función que calcula el área de un círculo.
+     * @param r Radio del círculo.
+     * @return Devuelve el área del círculo.
+     */
     public static double circleArea(double r) {
         return Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Función que calcula el área de una esfera.
+     * @param r Radio de la Esfera.
+     * @return Devuelve el área de la esfera
+     */
     public static double sphereArea(double r) {
         return 4 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Función que calcula el volumen de una esfera a partir del radio
+     * @param r Radio de la esfera
+     * @return El volumen de la esfera
+     */
     public static double sphereVolume(double r) {
         return (4/3) * Math.PI * Math.pow(r,3);
     }
 
+    /**
+     * Descripción: Función que especificando la moneda, convierte una cantidad de dinero a dólares.
+     * @param quantity Cantidad de dinero
+     * @param currency Divisa: Solo acepta BOB o MXN
+     * @return Devuelve el resultado de la conversión a US$
+     */
     public static double convertToDolar(double quantity, String currency) {
         switch (currency) {
             case "BOB":
