09f471f31d883f63130e248da46497db8cb65d22
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-08 12:13:50 -0400

    Ciclos For anidados

--- src/Arrays.java
+++ src/Arrays.java
@@ -23,10 +23,10 @@ public class Arrays {
         androidVersions[2] = "Cupcake";
         androidVersions[3] = "Donut";
 
-        System.out.println(androidVersions[0]);
-        System.out.println(androidVersions[1]);
-        System.out.println(androidVersions[2]);
-        System.out.println(androidVersions[3]);
+        for (int i = 0; i < androidVersions.length; i++) {
+            System.out.println(androidVersions[i]);
+        }
+
 
         System.out.println();
         System.out.println();
@@ -40,20 +40,38 @@ public class Arrays {
         cities[3][0] = "Bolivia";
         cities[3][1] = "CBB";
 
-        System.out.println(cities[0][0]);
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+
+      /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
 
         String[][][][] animals = new String[2][3][2][2];
         animals[1][0][0][1] = "Monkey";
 
         System.out.println();
         System.out.println();
-        System.out.println(animals[1][0][0][1]);
+        //System.out.println(animals[1][0][0][1]);
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <= 1; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+
+                }
+
+            }
+        }
     }
 }
