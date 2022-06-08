fdacf9646097214de19e1d95322752810550ba7d
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-08 11:42:23 -0400

    Declarando Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String daysOfTheWeek[] = new String[7];
+
+        String[][] cities = new String[4][2]; // 4 * 2 = 8 elementos
+        /*
+        *+--------------------+
+        *| Country | City     |
+        *----------------------
+        *| Bolivia | CBB      |
+        *| Bolivia | SCZ      |
+        *| Mexico  | CDMX     |
+        *| Colombia| Bogota   |
+        *+---------------------
+         */
+
+        int[][][] numbers = new int[2][2][2];
+        int[][][][] numbers4 = new int[2][2][2][2];
+    }
+}
