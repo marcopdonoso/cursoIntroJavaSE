9fd9af2d6f2a10ad3b3033ca6d362d202cd75604
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-08 12:00:18 -0400

    Indices y busqueda de elementos en Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -17,5 +17,43 @@ public class Arrays {
 
         int[][][] numbers = new int[2][2][2];
         int[][][][] numbers4 = new int[2][2][2][2];
+
+        androidVersions[0] = "Apple Pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);
+
+        System.out.println();
+        System.out.println();
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Bogota";
+        cities[1][0] = "Mexico";
+        cities[1][1] = "CDMX";
+        cities[2][0] = "Bolivia";
+        cities[2][1] = "SCZ";
+        cities[3][0] = "Bolivia";
+        cities[3][1] = "CBB";
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
+
+        System.out.println();
+        System.out.println();
+        System.out.println(animals[1][0][0][1]);
     }
 }
